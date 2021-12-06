0c2ea9bedb8dc203bbf93979f4efa0a3354a5017
jaapwalhout <jaap_w@yahoo.com>
2021-12-06 11:40:27 +0100

    day 6

---
+++ 2021/06.R
@@ -0,0 +1,47 @@
+
+#---------
+# part one
+#---------
+
+# d06 <- scan("2021/06_data_test.txt", sep = ",", what = integer())
+d06 <- scan("2021/06_data.txt", sep = ",", what = integer())
+
+res <- vector(mode = "list", length = 81)
+res[[1]] <- d06
+
+for (i in 1:80) {
+  res[[i+1]] <- res[[i]] - 1L
+  s <- sum(res[[i]] == 0L)
+  if(s > 0) {
+    res[[i+1]][res[[i]] == 0L] <- 6L
+    res[[i+1]] <- c(res[[i+1]], rep(8, s))
+  }
+}
+length(res[[81]])
+
+
+#---------
+# part two
+#---------
+
+options(scipen = 99)
+
+# d06 <- scan("2021/06_data_test.txt", sep = ",", what = integer())
+d06 <- scan("2021/06_data.txt", sep = ",", what = integer())
+
+nums <- as.numeric(0:8)
+counts <- as.numeric(rep(0, 9))
+
+tmp_counts <- c(table(d06))
+
+counts[match(names(tmp_counts), nums)] <- unname(tmp_counts)
+
+for (i in 1:256) {
+  tmp_counts <- counts[c(2:9,1)]
+  tmp_counts[7] <- tmp_counts[7] + counts[1]
+  counts <- tmp_counts
+}
+
+sum(counts)
+
+
